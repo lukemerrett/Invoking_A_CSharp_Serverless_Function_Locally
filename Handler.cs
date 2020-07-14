2f172c94b7b9f9548eebc5725c78d4fbca316ee0
Luke Merrett <luke.merrett@just-eat.com>
2020-07-14 14:46:29 +0100

    Simplified handler

--- Handler.cs
+++ Handler.cs
@@ -24,8 +24,6 @@ namespace AwsDotnetCsharp
 
     public class Request
     {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+      public string Value {get; set;}
     }
 }
